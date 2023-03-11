0bcb4420bf1ed0f6a45054c94adc56fd640b49a8
Ronald Mindiola <ronaldmindiola@live.com>
2023-03-11 12:19:37 -0500

    NavList

--- src/components/atoms/NavList.jsx
+++ src/components/atoms/NavList.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-/* import { Link } from "react-router-dom";
-import {
-  Box,
-  Button
-} from "@mui/material"; */
-//import config from "../../config.json";
+import { Link } from "react-router-dom";
+import { Box } from "@mui/material";
+import config from "../../config.json";
+
 
 const NavList = () => {
-  //const { pages } = config;
+  const { pages } = config;
   return (
     <>
-      {/* <Box sx={{ flexGrow: 1, display: { xs: "none", sm: "flex" }, spacing:2 }}>
+      <Box sx={{ display: { xs: "none", sm: "flex" }, spacing:2 }}>
         {pages.map((page) => (
-          <Button
+          <Box
             underline="hover"
             key={page.label}
             to={page.path}
             sx={{
               color: "gainsboro",
-              my: 0,
+              mx: 1,
               display: "block",
               textTransform: "capitalize",
               fontSize: 16,
@@ -28,9 +26,9 @@ const NavList = () => {
             }}
           >
             {page.label}
-          </Button>
+          </Box>
         ))}
-      </Box> */}11
+      </Box>
       
     </>
   );
